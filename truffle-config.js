fe8adf7ca311567ba713dd08a3705f096381f84c
Emmzyben <you@example.com>
2023-03-15 14:48:25 +0100

    commit

--- truffle-config.js
+++ truffle-config.js
@@ -86,7 +86,7 @@ module.exports = {
       network_id: 5,
       gas: 4465030,
       confirmations: 1,    // # of confirmations to wait between deployments. (default: 0)
-       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+       timeoutBlocks: 2000,  // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
      }
     //
